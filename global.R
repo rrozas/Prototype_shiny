a3306024d548ed2562c0d251534909cc7000ed16
rrozas <rozas.rony@gmail.com>
2015-02-11 10:21:06 +0100

    ajout de global.R

---
+++ global.R
@@ -0,0 +1,13 @@
+require(shiny)
+require(googleVis)
+require(xlsx)
+setwd("/Users/rrozas/Documents/Renault")
+features <-  read.xlsx(file = 'Data prototype Renault.xlsx', sheetIndex = 1)
+roc <-  read.xlsx(file = 'Data prototype Renault.xlsx', sheetIndex = 2)
+alerts <-  read.xlsx(file = 'Data prototype Renault.xlsx', sheetIndex = 3)
+meteo <-  read.xlsx(file = 'Data prototype Renault.xlsx', sheetIndex = 4)
+km <-  read.xlsx(file = 'Data prototype Renault.xlsx', sheetIndex = 5)
+date_fab <-  read.xlsx(file = 'Data prototype Renault.xlsx', sheetIndex = 6)
+map <-  read.xlsx(file = 'Data prototype Renault.xlsx', sheetIndex = 7)
+words <-  read.xlsx(file = 'Data prototype Renault.xlsx', sheetIndex = 8)
+AUC <- sum(roc$value_ROC)/100
